eb81e4a16278504014ce0bc66c002b51aca4b2ac
Walker Hale IV <walker.hale.iv@gmail.com>
2017-08-15 09:43:01 -0500

    Fix test case 4

--- test/test_4.sh
+++ test/test_4.sh
@@ -7,24 +7,28 @@ rm -rf "$TARGET"
 mkdir -p "$TARGET"
 
 cd "$TARGET"
+
 echo hello world a > input_a
 echo hello world b > input_b
+
 md5sum input_a > input_a.md5
 md5sum input_a > input_b.md5  # Deliberate error!
-bgzip input_?
+
+bgzip input_a
+bgzip input_b
 
 cd "$ROOT"
-./bgzip_md5.py -cv "$TARGET"/input_a.gz  2> "$TARGET"/test_4a.log
-if [[ ! $? ]]; then
+./bgzip_md5.py -cv "$TARGET"/input_a  2> "$TARGET"/test_4a.log
+if [[ $? != 0 ]]; then
     echo "got unexpected error; see test_4a.log"
 fi
 
-./bgzip_md5.py -cv "$TARGET"/input_b.gz  2> "$TARGET"/test_4b.log
-if [[ $? ]]; then
+./bgzip_md5.py -cv "$TARGET"/input_b  2> "$TARGET"/test_4b.log
+if [[ $? == 0 ]]; then
     echo "did not get expected error; see test_4b.log"
 fi
 
-./bgzip_md5.py -cv "$TARGET"/input_?.gz  2> "$TARGET"/test_4ab.log
-if [[ $? ]]; then
-    echo got zero result in test_4ab
+./bgzip_md5.py -cv "$TARGET"/input_{a,b}  2> "$TARGET"/test_4ab.log
+if [[ $? == 0 ]]; then
+    echo "did not get expected error; see test_4ab.log"
 fi
