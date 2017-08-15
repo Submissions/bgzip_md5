8a09323582b2bc8f28ba36f0bb97cb5d434960ec
Walker Hale IV <walker.hale.iv@gmail.com>
2017-08-15 14:06:13 -0500

    Check logged errors in test case 4

--- test/test_4.sh
+++ test/test_4.sh
@@ -22,13 +22,22 @@ cd "$ROOT"
 if [[ $? != 0 ]]; then
     echo "got unexpected error; see test_4a.log"
 fi
+if [ $(cut -d' ' -f3- target/test_4/test_4a.log | grep ^ERROR | wc -l) -ne 0 ]; then
+    echo "wrong number of ERROR lines in test_4a.log"
+fi
 
 ./bgzip_md5.py -cv "$TARGET"/input_b  2> "$TARGET"/test_4b.log
 if [[ $? == 0 ]]; then
     echo "did not get expected error; see test_4b.log"
 fi
+if [ $(cut -d' ' -f3- target/test_4/test_4b.log | grep ^ERROR | wc -l) -ne 1 ]; then
+    echo "wrong number of ERROR lines in test_4b.log"
+fi
 
 ./bgzip_md5.py -cv "$TARGET"/input_{a,b}  2> "$TARGET"/test_4ab.log
 if [[ $? == 0 ]]; then
     echo "did not get expected error; see test_4ab.log"
+fi
+if [ $(cut -d' ' -f3- target/test_4/test_4ab.log | grep ^ERROR | wc -l) -ne 1 ]; then
+    echo "wrong number of ERROR lines in test_4ab.log"
 fi
