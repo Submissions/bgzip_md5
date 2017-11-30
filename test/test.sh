bf9ad189c9d1913cc2477dd764b6a3783d1a5ed6
Walker Hale IV <walker.hale.iv@gmail.com>
2017-11-30 15:13:38 -0600

    Ensure target directory exists before use

--- test/test.sh
+++ test/test.sh
@@ -5,9 +5,9 @@
 # TODO: test -b option
 
 rm -rf target/test.log
+mkdir -p target  # in case target already exists
 
 for t in test/test_?.sh; do
     echo $t | tee -a target/test.log
-    mkdir -p target
     $t 2>>target/test.log
 done
